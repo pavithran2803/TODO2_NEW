adbb0e16440c9124c0df0cc9d39562830c8882ce
Your Name <you@example.com>
2021-10-11 18:59:45 +0530

    path module

---
+++ classfolder/free.js
@@ -0,0 +1,7 @@
+const path=require('path')
+// console.log(path.resolve('./app.js'))
+// console.log(path.relative('../','./app.js'))
+console.log(path.extname('./app.js'))
+console.log(path.dirname('./app.js'))
+console.log(__dirname)
+console.log(path.join(__dirname,'app.js'))
